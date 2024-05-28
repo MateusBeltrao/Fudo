de14c22e947f46c83411a99ddf92e1af664ee86a
Mateus Beltrão <mateusbeltrao2019@gmail.com>
2024-05-28 08:28:48 -0300

    hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,19 +1,49 @@
 import Image from 'next/image'
 import css from '../styles/Hero.module.css'
 import Cherry from '../assets/Cherry.png'
+import {UilPhone} from '@iconscout/react-unicons'
+import Pizza1 from '../assets/p1.jpg'
+import HeroImage from '../assets/HeroImage.png'
+
 export default function Hero() {
-    return(
-        <div className='css.container'>
-            
-            <div className={css.container}>
+    return (
+        <div className={css.container}>
+            <div className={css.left}>
+
                 <div className={css.cherryDiv}>
                     <span>More than Faster</span>
                     <Image src={Cherry} alt='' width={40} height={25}></Image>
                 </div>
+
+                <div className={css.heroText}>
+                    <span>Be The Fastest</span>
+                    <span>In Delivering</span>
+                    <span>
+                        Your <span style={{ color: "var(--themeRed" }}>Pizza</span>
+                    </span>
+                </div>
+
+                <span className={css.miniText}>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                </span>
+
+                <button className={`btn ${css.btn}`}>
+                    Get Started
+                </button>
             </div>
 
-            <div className={css.rightSide}>
-                21
+
+            <div className={css.right}>
+                <div className={css.imageContainer}>
+                    <Image src={HeroImage} alt='' layout='intrinsic'></Image>
+                </div>
+
+                <div className={css.ContactUs}>
+                    <span>Contact us</span>
+                    <div>
+                        <UilPhone color="white"></UilPhone>
+                    </div>
+                </div>
             </div>
         </div>
     )
